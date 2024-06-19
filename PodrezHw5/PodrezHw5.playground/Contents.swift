61455131b72cd61b67f00fa18d83731d3c5770fa
PodrezOleg <bolynita@icloud.com>
2024-06-19 18:15:02 +0300

    Task 2 improved

--- PodrezHw5/PodrezHw5.playground/Contents.swift
+++ PodrezHw5/PodrezHw5.playground/Contents.swift
@@ -11,21 +11,21 @@ enum seasons {
 
 //2. Написать функцию, которая будет принимать номер месяца (Int) и возвращать enum с временем года этого месяца.
 
-func matchSeason (month: Int) -> String {
+func matchSeasons(month:Int) -> seasons? {
     switch month {
     case 12, 1, 2:
-        return "Зима"
+        return.winter
     case 3, 4, 5:
-        return "Весна"
+        return.spring
     case 6, 7, 8:
-        return "Лето"
+        return.summer
     case 9, 10, 11:
-        return "Осень"
+        return.fall
     default:
-        return "ошибка ввода данных"
+        return nil
     }
 }
-print(matchSeason(month: 9))
+print(matchSeasons(month: 15) ?? "Wrong number of month")
 
 
 //3. Написать метод, который принимает variadic parameter String?. Метод возвращает количество nil объектов и печатает в консоль одну строку всех объединенных не nil объектов.
