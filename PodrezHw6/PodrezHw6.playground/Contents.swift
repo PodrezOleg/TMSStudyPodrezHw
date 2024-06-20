f420f607221d3c3aa29eb12aa7416708017254a6
PodrezOleg <bolynita@icloud.com>
2024-06-21 00:53:38 +0300

    Added description of Task 1

--- PodrezHw6/PodrezHw6.playground/Contents.swift
+++ PodrezHw6/PodrezHw6.playground/Contents.swift
@@ -1,10 +1,13 @@
 import Cocoa
 
+//1. Создайте два массива. Первый от 0 до 14, второй от 14 до 30. Объедините их в один массив.
+
 let Arr = Array(1...14)
 let Arr2 = Array(15...30)
 
 var Arr3 = Arr + Arr2
 
 print(Arr3)
+
 
 
