247cee14c3c2c71fb302aaa0c7446391ff2d27c3
PodrezOleg <bolynita@icloud.com>
2024-06-20 22:31:09 +0300

    Task 4 done

--- PodrezHw5/PodrezHw5.playground/Contents.swift
+++ PodrezHw5/PodrezHw5.playground/Contents.swift
@@ -35,4 +35,20 @@ print(matchSeasons(month: 9) ?? "Wrong number of month")
       
       
 //3. Написать метод, который принимает variadic parameter String?. Метод возвращает количество nil объектов и печатает в консоль одну строку всех объединенных не nil объектов.
+func varidicParameterString (arrString: String?...) -> Int {
+    var count: Int = 0
+    var listOfString: [String] = []
+    for string in arrString {
+        if string != nil {
+            if let string {
+                listOfString.append(string)
+            }
+        } else {
+            count += 1
+        }
+    }
+    return count
+}
+//print(varidicParameterString(arrString: "Balu", "Maugli", nil, "Sharhan", nil, "Baghira", nil, "Tabaki"))
+print("Количество Nil составило \(varidicParameterString(arrString: "Maugli", nil, "Sharhan", "Tabaki", nil, nil)) шт.")
 
