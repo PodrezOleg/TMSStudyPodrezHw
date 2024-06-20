71e33c3696ee7b63bc908d3a55c3edf51f74464c
PodrezOleg <bolynita@icloud.com>
2024-06-21 01:08:39 +0300

    Added additional writing of Task 1

--- PodrezHw6/PodrezHw6.playground/Contents.swift
+++ PodrezHw6/PodrezHw6.playground/Contents.swift
@@ -9,5 +9,13 @@ var Arr3 = Arr + Arr2
 
 print(Arr3)
 
+let ArrArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+let ArrArray2 = [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
 
+var ArrArray3 = ArrArray + ArrArray2
 
+print(ArrArray3)
+
+//2. Создать функцию, которая принимает массив int. Возвести все Int в квадрат. Возвратить новый массив.
+
+func intSquare
