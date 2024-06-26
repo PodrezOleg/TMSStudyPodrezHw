ddef3624fcf610790d7021fb58a6bfa12c4bcf8c
PodrezOleg <bolynita@icloud.com>
2024-06-26 18:38:03 +0300

    Исправлены ошибки в задании 1 и улучшенно отображение адреса

--- PodrezHw7/PodrezHw7.playground/Contents.swift
+++ PodrezHw7/PodrezHw7.playground/Contents.swift
@@ -2,12 +2,12 @@ import  Foundation
 
 //1. Создать объект “Школа” со свойствами: массив учеников, название школы, адрес (адрес - новый объект с координатами x, y, street name) и директор (у директора новые поля: experience, рейтинг. Директор наследуется от класса Person (name:surname:age)).
 
-enum sex: String{
+enum Sex: String{
     case male = "Мужской"
     case female = "Женский"
 }
 
-enum rating: String  {
+enum Rating: String  {
     case poor = "Неудовлетворительно"
     case average = "Средний"
     case good = "Хороший"
@@ -15,30 +15,33 @@ enum rating: String  {
 }
 
 
-struct Address {
+struct Address: CustomStringConvertible {
     var longitude: Double
     var latitude: Double
     var streetName: String
+    
+    var description: String {
+        return "Улица \(streetName), Долгота: \(longitude), Широта: \(latitude)"
+    }
 }
 
 struct School {
     var students: [String]
     var schoolName: String
     var address: Address
-    var rating: rating
+    var rating: Rating
 
 }
 
 let schoolAddress = Address(longitude: 85.131, latitude: 621.1516, streetName: "Lenina 1")
-let school = School(students: ["Ivan", "Kiril", "Inga", "Mark"], schoolName: "Gimnasium 12", address: schoolAddress, rating: rating.exellent)
+let school = School(students: ["Ivan", "Kiril", "Inga", "Mark"], schoolName: "Gimnasium 12", address: schoolAddress, rating: Rating.exellent)
 let students = school.students
 
 print("Полный адрес школы \(schoolAddress)")
-//Странно выдает адрес
+
 print("Школа построена на улице \(school.address.streetName)")
 print("Студенты \(school.students)")
-// А тут имено в ковычках
-print(students)
+print("Студенты \(school.students.joined(separator: ","))")
 
 
 // 1.2 директор (у директора новые поля: experience, рейтинг. Директор наследуется от класса Person (name:surname:age)).
@@ -47,12 +50,12 @@ class Person {
     let name: String
     let surname: String
     let age: Int
-    let sex: sex
+    let sex: Sex
     
     init(name: String,
          surname: String,
          age: Int,
-         sex: sex
+         sex: Sex
     ) {
         self.name = name
         self.surname = surname
@@ -66,15 +69,15 @@ class Person {
 
 class Director: Person {
     let expirience: Int
-    let rating: rating
+    let rating: Rating
     
     init(
         expirience: Int,
-        rating: rating,
+        rating: Rating,
         name: String,
         surname: String,
         age: Int,
-        sex: sex
+        sex: Sex
     ) {
         self.expirience = expirience
         self.rating = rating
@@ -82,12 +85,12 @@ class Director: Person {
     }
 }
 
-let director = Director(expirience: 10, rating: .average, name: "Ivan", surname: "Ivanovich", age: 50, sex: .male)
+let director = Director(expirience: 10, rating: Rating.exellent, name: "Ivan", surname: "Ivanovich", age: 50, sex: .male)
+let chooseSchool = School(students: ["Кира", "Кейдж", "Мит", "Онага", "Рэйко"], schoolName: "School 1", address:  Address(longitude: 87.465, latitude: 11.1654, streetName: "Lenina 1"), rating: Rating.exellent)
 
-let chooseSchool = School(students: ["Кира", "Кейдж", "Мит", "Онага", "Рэйко"], schoolName: "School 1", address:  Address(longitude: 87.465, latitude: 11.1654, streetName: "Lenina 1"), rating: rating.exellent)
                           
-print("ФИО диреткора \(director.name) \(director.surname), в возрасте \(director.age) лет, с опытом более \(director.expirience) лет, пол \(sex.male.rawValue)")
-print(chooseSchool)
+print("ФИО диреткора \(director.name) \(director.surname), в возрасте \(director.age) лет, с опытом более \(director.expirience) лет, пол \(Sex.male.rawValue)")
+print("Выбирая школу мы учитывали: опыт \(director.expirience) лет и его рейтинг \(director.rating), а так же близость к школе,  \(Address(longitude: 21.123, latitude: 555.555, streetName: "Lenina 4"))")
 
 
 //2.
