f586b13c0293195447dad2cb864328011711b4be
PodrezOleg <bolynita@icloud.com>
2024-06-29 19:26:22 +0300

    Some issues with privet let

--- PodrezHw7/PodrezHw7.playground/Contents.swift
+++ PodrezHw7/PodrezHw7.playground/Contents.swift
@@ -38,6 +38,20 @@ class Director: Person {
     }
 }
 
+class Student: Person {
+  private var grade: Int
+    var subjects: [(SchoolSubject, Int)]
+    
+    init(name: String, surname: String, age: Int, grade: Int, subjects: [(SchoolSubject, Int)] = []) {
+        self.grade = grade
+        self.subjects = subjects
+        super.init(name: name, surname: surname, age: age)
+    }
+        func getGrade() -> Int {
+            return grade
+    }
+ }
+
 struct Address {
     var longitude: Double
     var latitude: Double
@@ -67,18 +81,45 @@ let address = Address(longitude: 85.131, latitude: 621.1516, street: "Lenina 1")
 let school = School(students: ["Ivan", "Kiril", "Inga", "Mark"], schoolName: "Gimnasium 12", address: address, director: director)
 
 
+
 print("Школа построена на улице \(school.address.street)")
 print("Студенты \(school.students)")
 print(address.description)
+print("Директор \(school.director.name) \(school.director.surname) \(school.director.experience) лет опыта \(Rating.poor.rawValue) ")
 
 
-class Student: Person {
-    let grade: Int
-    var subjects: [(SchoolSubject, Int)]
-    
-    init(grade: Int, subjects: [(SchoolSubject, Int)]) {
-        self.grade = grade
-        self.subjects = subjects
-        super.init(name: name, surname: surname, age: age)
+
+
+//2.1 У ученика хранить имя, фамилию, номер класса, кортеж “название предмета - оценка”. Ученик наследуется от Person.
+let studentMasha = Student(name: "Masha",
+                           surname: "Smith",
+                           age: 19, 
+                           grade: 5,
+                           subjects: [(.math, 10), (.law, 6)]
+)
+let studentIhar = Student(name: "Ihar",
+                          surname: "Alexovich",
+                          age: 20,
+                          grade: 6,
+                          subjects: [(SchoolSubject.law, 10), (SchoolSubject.math, 10), (SchoolSubject.psyhology, 10)]
+)
+
+let gradeMasha = studentMasha.getGrade()
+let gradeIhar = studentIhar.getGrade()
+
+
+print("Студент \(studentMasha.name) \(studentMasha.surname) на \(studentMasha.grade) курс в возрасте \(studentMasha.age) лет и получила следующие оценки:")
+for subject in studentMasha.subjects {
+    switch subject.0 {
+    case .math:
+        print(" \(SchoolSubject.math.rawValue) - оценка \(subject.1)")
+    case.law:
+        print(" \(SchoolSubject.law.rawValue) - оценка \(subject.1)")
+    default:
+        break
     }
 }
+
+print("Студент \(studentIhar.name) \(studentIhar.surname) перевелся на \(studentIhar.grade) курс в возрасте \(studentIhar.age)")
+
+
