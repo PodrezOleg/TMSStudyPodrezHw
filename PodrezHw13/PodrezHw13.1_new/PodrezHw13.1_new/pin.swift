d9e5d380b94dd40c09f238262eebcda112c5ada8
PodrezOleg <bolynita@icloud.com>
2024-08-14 18:50:41 +0300

    Added images to TableView and some structure

---
+++ PodrezHw13/PodrezHw13.1_new/PodrezHw13.1_new/pin.swift
@@ -0,0 +1,19 @@
+//
+//  pin.swift
+//  PodrezHw13.1_new
+//
+//  Created by Oleg Podrez on 14.08.24.
+//
+
+import UIKit
+
+extension UIView {
+    func pin(to superView: UIView) {
+        translatesAutoresizingMaskIntoConstraints = false
+        topAnchor.constraint(equalTo: superView.topAnchor).isActive = true
+        leadingAnchor.constraint(equalTo: superView.leadingAnchor).isActive = true
+        trailingAnchor.constraint(equalTo: superView.trailingAnchor).isActive = true
+        bottomAnchor.constraint(equalTo: superView.bottomAnchor).isActive = true
+        
+    }
+}
