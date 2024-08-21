2adfbe6dbbb96cfbf10a63d3c85f486806b5baaf
PodrezOleg <bolynita@icloud.com>
2024-08-21 21:00:51 +0300

    no message

---
+++ PodrezHw15/PodrezHw15/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PodrezHw15
+//
+//  Created by Oleg Podrez on 21.08.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     
+    }
+
+
+}
+
