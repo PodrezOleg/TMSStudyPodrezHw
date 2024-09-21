a30ff97ad7e1957ab13ed3d88f46981471c3f8c3
PodrezOleg <bolynita@icloud.com>
2024-09-22 00:32:06 +0300

    HW 25 done

---
+++ PodrezHw25/PodrezHw25.playground/Contents.swift
@@ -0,0 +1,49 @@
+import Cocoa
+
+class PersonBig {
+    var name: String
+    var sex: String
+    var age: Int
+   weak var apartment: Apartment?
+
+    init(name: String, sex: String, age: Int, apartment: Apartment? = nil) {
+        self.name = name
+        self.sex = sex
+        self.age = age
+        self.apartment = apartment
+        print("PersonBig \(name) init")
+    }
+    
+    deinit {
+        print ("PersonBig \(name) deinit")
+    }
+}
+
+class Apartment {
+    var address: String
+    var number: Int
+  weak var resident: PersonBig?
+    
+    
+    init(address: String, number: Int, resident: PersonBig? = nil) {
+        self.address = address
+        self.number = number
+        self.resident = resident
+        print ("Apartment \(address) \(number) initi")
+    }
+    deinit {
+        print("Apartment \(address) \(number) deinit")
+    }
+}
+
+var manson: PersonBig? = PersonBig(name: "Manson", sex: "Male", age: 55)
+var apartment127: Apartment? = Apartment(address: "Lenina 99", number: 101)
+
+manson?.apartment = apartment127
+apartment127?.resident = manson
+
+print("\(manson?.name ?? "No name") lives at \(manson?.apartment?.address ?? "No address")")
+print("\(apartment127?.resident?.name ?? "No resident") lives in apartment \(apartment127?.number ?? 0)")
+
+manson = nil
+apartment127 = nil
