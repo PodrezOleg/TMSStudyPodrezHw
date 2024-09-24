870c32cc1b78c6bc12d5e85becd5ed0f5d4b8212
PodrezOleg <bolynita@icloud.com>
2024-09-24 21:36:43 +0300

    put it in decimel format

--- PodrezHw27/PodrezHw27/ViewController.swift
+++ PodrezHw27/PodrezHw27/ViewController.swift
@@ -12,21 +12,30 @@ class ViewController: UIViewController {
     class BankAccount {
         private var balance: Double = 0
         private let semaphore = DispatchSemaphore(value: 1)
+        private let numberFormatter: NumberFormatter = {
+            let formatter = NumberFormatter()
+            formatter.numberStyle = .decimal
+            formatter.maximumFractionDigits = 2
+            formatter.minimumFractionDigits = 2
+            return formatter
+        }()
         
         func deposit(amount: Double) {
+            let roundedAmount = round(amount * 100) / 100.0
             semaphore.wait()
-            balance += amount
-            print("Пополнил счет на: \(amount). Текущий баланс: \(balance)")
+            balance += roundedAmount
+            print("Пополнил счет на: \(roundedAmount). Текущий баланс: \(balance)")
             semaphore.signal()
         }
         
         func withdraw(amount: Double) {
+            let roundedAmount = round(amount * 100) / 100.0
             semaphore.wait()
             if balance >= amount {
                 balance -= amount
-                print("Снял: \(amount). Текущий баланс: \(balance)")
+                print("Снял: \(roundedAmount). Текущий баланс: \(balance)")
             } else {
-                print("Хочу снять: \(amount). Недостаточно средств.Текущий баланс: \(balance)")
+                print("Хочу снять: \(roundedAmount). Недостаточно средств.Текущий баланс: \(balance)")
             }
             semaphore.signal()
         }
