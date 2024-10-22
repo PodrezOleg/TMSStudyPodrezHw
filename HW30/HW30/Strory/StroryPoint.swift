171a923ec11a4480f19578126adf0569a189b139
PodrezOleg <bolynita@icloud.com>
2024-10-22 03:54:56 +0300

    HW30 is Done the Game can be improved, add some twist plots, new characters, and some context. Main logic is ready and can be easily expanded. and, of course, fix the bugs.

--- HW30/HW30/Strory/StroryPoint.swift
+++ HW30/HW30/Strory/StroryPoint.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 struct StoryPoint {
+    let id: Int
     let text: String
     let choices: [Choice]
     let backGroundView: String
@@ -22,20 +23,21 @@ struct Choice {
 let storyPoints: [StoryPoint] = [
     // Развилка 1: Начало истории
     StoryPoint(
+        id: 201,
         text: "Главный герой только что проснулся после процедуры в «Виртуме». Взрыв раздаётся неподалёку, дверь распахивается, и в комнату врываются вооружённые агенты.",
         choices: [
             Choice(
                 text: "Бежать, пытаясь сохранить свою жизнь.",
-                nextStoryPointID: 1,
+                nextStoryPointID: 202,
                 explanation: "{name} мчится по переулкам, но за ним следят как агенты, так и подозрительные люди в гражданской одежде."
             ),
             Choice(
                 text: "Вступить в бой, полагая, что он — агент сопротивления.",
-                nextStoryPointID: 2,
+                nextStoryPointID: 202,
                 explanation: "Инстинкты не подводят его, и он побеждает агентов."
             ),
             Choice(text: "Сдаться и остаться на месте.",
-                   nextStoryPointID: 3,
+                   nextStoryPointID: 208,
                    explanation: "Надеясь, что агенты его не тронут. Однако они захватывают его и увозят в неизвестное место, где его память полностью стирают.")
         ],
         backGroundView: "HomeBackground"
@@ -43,16 +45,17 @@ let storyPoints: [StoryPoint] = [
     
     //Развилка 2: Квартира
     StoryPoint(
+        id: 202,
         text: "После событий в «Виртуме», {name} возвращается в квартиру и находит старый коммуникатор с сообщением, которое может объяснить его жизнь.",
         choices: [
             Choice(text: "Воспроизвести сообщение.",
-                   nextStoryPointID: 4,
+                   nextStoryPointID: 203,
                    explanation: "Он узнаёт больше о своей жизни как агента сопротивления и начинает подозревать, что корпорация контролирует его воспоминания."),
             Choice(text: "Уничтожить коммуникатор.",
-                   nextStoryPointID: 5,
+                   nextStoryPointID: 203,
                    explanation: "{name} считает это ловушкой и уничтожает устройство, продолжая свои поиски правды."),
             Choice(text: "Игнорировать находку",
-                   nextStoryPointID: 6,
+                   nextStoryPointID: 203,
                    explanation: "и оставить коммуникатор как есть. Это приводит к тому, что Марк продолжает свои действия вслепую, упуская важную информацию.")
         ],
         backGroundView: "KvartalBackGround"
@@ -60,16 +63,17 @@ let storyPoints: [StoryPoint] = [
     
     // Развилка 3: Знакомство с загадочной женщиной
     StoryPoint(
+        id: 203,
         text: "После бегства {name} сталкивается с женщиной, утверждающей, что она его жена, и они вместе сражались против корпорации.",
         choices: [
             Choice(text: "Довериться ей и пойти с ней.",
-                   nextStoryPointID: 7,
+                   nextStoryPointID: 204,
                    explanation: "Женщина рассказывает {name} о его прошлом и о том, что он должен вспомнить, чтобы победить корпорацию."),
             Choice(text: "Остаться одному",
-                   nextStoryPointID: 8,
+                   nextStoryPointID: 204,
                    explanation: "сомневаясь в её словах. {name} решает идти своим путём, полагаясь только на себя."),
             Choice(text: "Атаковать её, подозревая, что она — агент корпорации.",
-                   nextStoryPointID: 9, explanation: "Поддавшись паранойе (возможно, из-за того, что он игнорировал коммуникатор на второй развилке), {name} пытается напасть на женщину. Она, защищаясь, убивает его, считая его врагом. Это приводит к смерти {name} — окончанию, где он погибает от собственной неуверенности и недоверия.")
+                   nextStoryPointID: 208, explanation: "Поддавшись паранойе (возможно, из-за того, что он игнорировал коммуникатор на второй развилке), {name} пытается напасть на женщину. Она, защищаясь, убивает его, считая его врагом. Это приводит к смерти {name} — окончанию, где он погибает от собственной неуверенности и недоверия.")
         ],
         
         backGroundView: "HomeWomenBackGround"
@@ -77,16 +81,17 @@ let storyPoints: [StoryPoint] = [
     
     // Развилка 4: Предатель среди союзников
     StoryPoint(
+        id: 204,
         text: " {name} доверился женщине, они оказываются в укрытии сопротивления. Однако среди их союзников оказывается предатель.",
         choices: [
             Choice(text: "Обвинить одного из членов группы",
-                   nextStoryPointID: 10,
+                   nextStoryPointID: 205,
                    explanation: "Основываясь на интуиции. Марк правильно угадывает, кто предатель, и предотвращает заговор."),
             Choice(text: "Не предпринимать никаких действий",
-                   nextStoryPointID: 11,
+                   nextStoryPointID: 205,
                    explanation: "опасаясь ошибиться. В этом случае предатель выдаёт их планы, и группа оказывается в опасности."),
             Choice(text: "Попытаться сбежать в одиночку, не доверяя никому",
-                   nextStoryPointID: 12,
+                   nextStoryPointID: 207,
                    explanation: "Если {name} выбирает сбежать, он оказывается один в туннелях и погибает в ловушке, устроенной корпорацией, потому что у него не было поддержки от союзников.")
         ],
         backGroundView: "DungenBackground"
@@ -94,48 +99,52 @@ let storyPoints: [StoryPoint] = [
     
     // Развилка 5: Устройство контроля воспоминаний
     StoryPoint(
+        id: 205,
         text: "После долгих испытаний {name} они находят устройство, контролирующее воспоминания всех жителей.",
         choices: [
             Choice(text: "Деактивировать устройство",
-                   nextStoryPointID: 13,
-                    explanation: "чтобы освободить людей, рискуя непредсказуемыми последствиями. В результате корпорация теряет контроль, и [name] восстанавливает свою личность."),
+                   nextStoryPointID: 206,
+                    explanation: "чтобы освободить людей, рискуя непредсказуемыми последствиями. В результате корпорация теряет контроль, и {name} восстанавливает свою личность."),
             Choice(text: "Не трогать устройство",
-                   nextStoryPointID: 14,
+                   nextStoryPointID: 207,
                    explanation: "опасаясь катастрофы. В этом случае устройство остаётся под контролем корпорации, и мир погружается в ещё больший хаос."),
             Choice(text: "Попытаться уничтожить устройство силой",
-                   nextStoryPointID: 15,
+                   nextStoryPointID: 208,
                    explanation: "не разобравшись в его работе. Если {name} решает действовать импульсивно, устройство взрывается, убивая его и всех вокруг. Это приводит к смертельной концовке, где его необдуманные действия приводят к гибели.")
         ],
         backGroundView: "DesertBackground"
     ),
     // Хорошая концовка
     StoryPoint(
+        id: 206,
         text: "Поздравляем, {name}, ты победил корпорацию и восстановил свою личность!",
         choices: [
-            Choice(text: "Просмотр таблицы лидеров", nextStoryPointID: 101, explanation: ""),
-            Choice(text: "Начать заново", nextStoryPointID: 102, explanation: "")
+            Choice(text: "Просмотр таблицы лидеров", nextStoryPointID: 101, explanation: "Сейчас тебе покажут мультик Ну погоди)) "),
+            Choice(text: "Начать заново", nextStoryPointID: 102, explanation: "Уверен?")
         ],
         backGroundView: "GoodEndingBackground"
     ),
 
     // Плохая концовка
     StoryPoint(
+        id: 207,
         text: "Корпорация захватила тебя и полностью стерла твои воспоминания. Ты потерпел поражение.",
         choices: [
-            Choice(text: "Просмотр таблицы лидеров", nextStoryPointID: 101, explanation: ""),
-            Choice(text: "Начать заново", nextStoryPointID: 102, explanation: "")
+            Choice(text: "Просмотр таблицы лидеров", nextStoryPointID: 101, explanation: "Ну че ты так ? )"),
+            Choice(text: "Начать заново", nextStoryPointID: 102, explanation: "Уверен?")
         ],
         backGroundView: "BadEndingBackground"
     ),
 
     // Смертельная концовка
     StoryPoint(
+        id: 208,
         text: "Ты погиб. Время твоей игры сохранено в таблице лидеров.",
         choices: [
-            Choice(text: "Просмотр таблицы лидеров", nextStoryPointID: 101, explanation: ""),
-            Choice(text: "Начать заново", nextStoryPointID: 102, explanation: "")
+            Choice(text: "Просмотр таблицы лидеров", nextStoryPointID: 101, explanation: "Бывет) "),
+            Choice(text: "Начать заново", nextStoryPointID: 102, explanation: "Уврен?")
         ],
-        backGroundView: "MountainsBacground"
+        backGroundView: "DeathEndingBackground"
     )
 
 ]
--- HW30/HW30/ViewController/NewGameStartViewController.swift
+++ HW30/HW30/ViewController/NewGameStartViewController.swift
@@ -12,7 +12,7 @@ class NewGameStartViewController: UIViewController {
     let storyStackView = UIStackView()
     let storyTextView = UITextView()
     var currentTextIndex = 0
-    var currentStoryPointID = 0
+    var currentStoryPointID = 201
     var playerName: String = "Fantom Player"
     var isShowingChoiceExplanation = false
     var selectedChoiceExplanation: String?
@@ -22,31 +22,40 @@ class NewGameStartViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        backgroundController.setupBlurBackground(for: view,
-                                                 style: .extraLight,
-                                                 alpha: LayoutConstants.blurAlpha)
+        backgroundController.setupBlurBackground(for: view, style: .extraLight, alpha: LayoutConstants.blurAlpha)
         backgroundController.setupBackground(for: view, imageName: "StoryBackground")
         
-        UINewStartViewController.setupUI(for: self,
-                                         storyTextView: storyTextView,
-                                         storyStackView: storyStackView)
-        UINewStartViewController.setupGestures(for: self,
-                                        nextSelector: #selector(handleNextSegmentTap),
-                                        previousSelector: #selector(handlePreviousSegmentSwipe))
+        UINewStartViewController.setupUI(for: self, storyTextView: storyTextView, storyStackView: storyStackView)
+        UINewStartViewController.setupGestures(for: self, nextSelector: #selector(handleNextSegmentTap), previousSelector: #selector(handlePreviousSegmentSwipe))
         
         displayPlayerIntro()
     }
-    //Добить путь к концовкам
+    
     @objc func handleNextSegmentTap() {
         if isShowingChoiceExplanation {
             isShowingChoiceExplanation = false
-            if let nextStoryPointID = selectedNextStoryPointID {
-                if nextStoryPointID >= 0 && nextStoryPointID < storyPoints.count {
-                    currentStoryPointID = nextStoryPointID
+            
+            if selectedNextStoryPointID == 101 {
+                endGameWithDeath()
+                } else if selectedNextStoryPointID == 102 {
+                    backToMainMenu() 
+                } else if let nextStoryPoint = findStoryPoint(by: selectedNextStoryPointID ?? -1) {
+                    currentStoryPointID = nextStoryPoint.id
                     displayCurrentStoryPoint()
                 } else {
-                    print("Ошибка: выбранный ID выходит за пределы массива storyPoints")
+                    print("Ошибка: ID следующей точки не найден.")
                 }
+            
+            if let tapGestures = view.gestureRecognizers?.filter({ $0 is UITapGestureRecognizer }) {
+                for gesture in tapGestures {
+                    view.removeGestureRecognizer(gesture)
+                }
+            }
+            if let nextStoryPointID = selectedNextStoryPointID, let nextStoryPoint = findStoryPoint(by: nextStoryPointID) {
+                currentStoryPointID = nextStoryPoint.id
+                displayCurrentStoryPoint()
+            } else {
+                print("Ошибка: ID следующей точки не найден.")
             }
         } else {
             displayNextSegment()
@@ -63,10 +72,7 @@ class NewGameStartViewController: UIViewController {
             return
         }
         
-        let textWithPlayerName = ReplaceName.replacePlayerName(
-            in: lorText[currentTextIndex],
-            with: playerName
-        )
+        let textWithPlayerName = ReplaceName.replacePlayerName(in: lorText[currentTextIndex], with: playerName)
         storyTextView.text = textWithPlayerName
     }
     
@@ -87,46 +93,38 @@ class NewGameStartViewController: UIViewController {
     }
     
     func displayCurrentStoryPoint() {
-        let currentStoryPoint = storyPoints[currentStoryPointID]
+        guard let currentStoryPoint = findStoryPoint(by: currentStoryPointID) else {
+            print("Ошибка: Точка с ID \(currentStoryPointID) не найдена")
+            return
+        }
         
-        let storyTextWithPlayerName = ReplaceName.replacePlayerName(
-            in: currentStoryPoint.text,
-            with: playerName
-        )
+        let storyTextWithPlayerName = ReplaceName.replacePlayerName(in: currentStoryPoint.text, with: playerName)
         storyTextView.text = storyTextWithPlayerName
         
-        // Проверяем, если это смертельная концовка!!!!!!!
-          if currentStoryPoint.text.contains("Ты погиб") {
-              GameTimer.shared.stop()
-              GameStateManager.shared.addLeaderboardEntry(playerName: playerName, time: gameTimer.elapsedTime)
-              print("Таймер остановлен. Время: \(gameTimer.elapsedTime) секунд. Результат добавлен в таблицу лидеров.")
-          }
+        if currentStoryPoint.text.contains("Ты погиб") {
+            GameTimer.shared.stop()
+            GameStateManager.shared.addLeaderboardEntry(playerName: playerName, time: gameTimer.elapsedTime)
+            print("Таймер остановлен. Время: \(gameTimer.elapsedTime) секунд. Результат добавлен в таблицу лидеров.")
+        }
+        
+        storyStackView.arrangedSubviews.forEach { $0.removeFromSuperview() }
         
         for choice in currentStoryPoint.choices {
             let choiceButton = UIButton()
-            let choiceTextWithPlayerName = ReplaceName.replacePlayerName(
-                in: choice.text,
-                with: playerName
-            )
+            let choiceTextWithPlayerName = ReplaceName.replacePlayerName(in: choice.text, with: playerName)
             choiceButton.setTitle(choiceTextWithPlayerName, for: .normal)
-            let explanationWithPlayerName = ReplaceName.replacePlayerName(
-                in: choice.explanation,
-                with: playerName
-            )
+            let explanationWithPlayerName = ReplaceName.replacePlayerName(in: choice.explanation, with: playerName)
             choiceButton.backgroundColor = UIColor.orange.withAlphaComponent(LayoutConstants.blurAlpha)
             choiceButton.layer.cornerRadius = LayoutConstants.cornerRadiusButton
-            
             choiceButton.titleLabel?.numberOfLines = 0
             choiceButton.titleLabel?.textAlignment = .center
             choiceButton.titleLabel?.minimumScaleFactor = 0.5
-            
             choiceButton.translatesAutoresizingMaskIntoConstraints = false
             
             NSLayoutConstraint.activate([
                 choiceButton.widthAnchor.constraint(equalToConstant: LayoutConstants.widthStroyButton),
                 choiceButton.heightAnchor.constraint(equalToConstant: LayoutConstants.heightStroyButton)
-            ]
-        )
+            ])
             
             choiceButton.accessibilityHint = explanationWithPlayerName
             choiceButton.tag = choice.nextStoryPointID
@@ -134,51 +132,44 @@ class NewGameStartViewController: UIViewController {
             storyStackView.addArrangedSubview(choiceButton)
         }
         
-        backgroundController.setupBackground(for: view,
-                                             imageName: currentStoryPoint.backGroundView)
+        backgroundController.setupBackground(for: view, imageName: currentStoryPoint.backGroundView)
     }
-    
+
     @objc func choiceSelected(_ sender: UIButton) {
         if let explanation = sender.accessibilityHint {
             selectedChoiceExplanation = explanation
             storyTextView.text = explanation
         }
-        
         isShowingChoiceExplanation = true
         selectedNextStoryPointID = sender.tag
-        
-        if selectedNextStoryPointID == 101 {
-            endGameWithDeath()
-        } else if selectedNextStoryPointID == 102 {
-            backToMainMenu()
-        } else {
-            displayCurrentStoryPoint()
-        }
-            return
-        
+
         storyStackView.arrangedSubviews.forEach { $0.removeFromSuperview() }
+
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleNextSegmentTap))
+        view.addGestureRecognizer(tapGesture)
+    }
+
+    func findStoryPoint(by id: Int) -> StoryPoint? {
+        return storyPoints.first(where: { $0.id == id })
     }
     
     @objc func saveCurrentGame() {
-           GameStateManager.shared.saveGame(playerName: playerName,
-                                            currentStoryPointID: currentStoryPointID,
-                                            elapsedTime: gameTimer.elapsedTime)
-           
-       }
+        GameStateManager.shared.saveGame(playerName: playerName, currentStoryPointID: currentStoryPointID, elapsedTime: gameTimer.elapsedTime)
+    }
        
-       @objc func loadSavedGame() {
-           if let savedGame = GameStateManager.shared.loadGame() {
-               self.playerName = savedGame.playerName
-               self.currentStoryPointID = savedGame.currentStoryPointID
-               GameTimer.shared.stop()
-               GameStateManager.shared.addLeaderboardEntry(playerName: playerName,
-                                                           time: gameTimer.elapsedTime)
-           }
-       }
+    @objc func loadSavedGame() {
+        if let savedGame = GameStateManager.shared.loadGame() {
+            self.playerName = savedGame.playerName
+            self.currentStoryPointID = savedGame.currentStoryPointID
+            GameTimer.shared.stop()
+            GameStateManager.shared.addLeaderboardEntry(playerName: playerName, time: gameTimer.elapsedTime)
+        }
+    }
     
     func endGameWithDeath() {
         GameTimer.shared.stop()
         GameStateManager.shared.addLeaderboardEntry(playerName: playerName, time: gameTimer.elapsedTime)
+        UserDefaults.standard.set(true, forKey: "endGameWithDeath")
         showLeaderboardScreen()
     }
     
