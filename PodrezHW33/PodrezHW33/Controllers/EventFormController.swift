30ed2a6a7cec22b92fda014eb48bf300cd1a4635
PodrezOleg <bolynita@icloud.com>
2024-10-31 00:19:32 +0300

    Logic issues simple UI ready Didn't manage to check local notifications

--- PodrezHW33/PodrezHW33/Controllers/EventFormController.swift
+++ PodrezHW33/PodrezHW33/Controllers/EventFormController.swift
@@ -11,9 +11,84 @@ import ContactsUI
 class EventFormController: UIViewController, CNContactPickerDelegate {
     var onAdd: ((Event) -> Void)?
     
+    private let titleTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Укажи событие"
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+    
+    private let datePicker: UIDatePicker = {
+        let picker = UIDatePicker()
+        picker.datePickerMode = .dateAndTime
+        return picker
+    }()
+    
+    private let useContactSwitch = UISwitch()
+    private var selectedContactName: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.backgroundColor = .white
+        title = "Добавить Событие"
         
+        setupUI()
+    }
+    
+    private func setupUI() {
+        let useContactLabel = UILabel()
+        useContactLabel.text = "Использовать контакт"
+        
+        let addEventButton = UIButton(type: .system)
+        addEventButton.setTitle("Добавить", for: .normal)
+        addEventButton.addTarget(self, action: #selector(addEvent), for: .touchUpInside)
+        
+        [titleTextField, datePicker, useContactLabel, useContactSwitch, addEventButton].forEach {
+            view.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        NSLayoutConstraint.activate([
+            titleTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            titleTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            titleTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            datePicker.topAnchor.constraint(equalTo: titleTextField.bottomAnchor, constant: 20),
+            datePicker.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            datePicker.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            useContactLabel.topAnchor.constraint(equalTo: datePicker.bottomAnchor, constant: 20),
+            useContactLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            
+            useContactSwitch.centerYAnchor.constraint(equalTo: useContactLabel.centerYAnchor),
+            useContactSwitch.leadingAnchor.constraint(equalTo: useContactLabel.trailingAnchor, constant: 10),
+            
+            addEventButton.topAnchor.constraint(equalTo: useContactSwitch.bottomAnchor, constant: 30),
+            addEventButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        ])
+        
+        useContactSwitch.addTarget(self, action: #selector(selectContact), for: .valueChanged)
+    }
+    
+    @objc private func selectContact() {
+        if useContactSwitch.isOn {
+            let contactPicker = CNContactPickerViewController()
+            contactPicker.delegate = self
+            present(contactPicker, animated: true, completion: nil)
+        }
+    }
+    
+    func contactPicker(_ picker: CNContactPickerViewController, didSelect contact: CNContact) {
+        selectedContactName = contact.givenName
+    }
+    
+    @objc private func addEvent() {
+        let eventTitle = (useContactSwitch.isOn && selectedContactName != nil) ? "День Рождения \(selectedContactName!)" : titleTextField.text ?? "Событие"
+        let newEvent = Event(title: eventTitle, date: datePicker.date, notificationInterval: 60 * 60 * 24) // уведомление за 1 день
+        
+        onAdd?(newEvent)
+        
+        navigationController?.popViewController(animated: true)
     }
 }
--- PodrezHW33/PodrezHW33/Controllers/EventListController.swift
+++ PodrezHW33/PodrezHW33/Controllers/EventListController.swift
@@ -8,42 +8,43 @@
 import UIKit
 
 class EventListController: UIViewController,  UITableViewDataSource, UITableViewDelegate {
-   
+    
     var events: [Event] = StorageManager.shared.loadEvents().sorted { $0.date < $1.date}
     
     private let tableView = UITableView()
     
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            
-            view.backgroundColor = .white
-            title = "Напомниналка"
-            
-            navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addEvent))
-            
-            tableView.dataSource = self
-            tableView.delegate = self
-            tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-            
-            view.addSubview(tableView)
-            tableView.translatesAutoresizingMaskIntoConstraints = false
-            
-            NSLayoutConstraint.activate([
-                tableView.topAnchor.constraint(equalTo: view.topAnchor),
-                tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-                tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-                tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
-            ])
-            
-            Notifications.shared.requestAuthorization()
-        }
-
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        title = "Напомниналка"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addEvent))
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+        
+        Notifications.shared.requestAuthorization()
+    }
+    
     @objc private func addEvent() {
         let vc = EventFormController()
-        vc.onAdd = { [week self] newEvent in
+        vc.onAdd = { [weak self] newEvent in
             self?.events.append(newEvent)
-            self?.events.sort { $0.date < $1.date}
-            StorageManager.shared.scheduelNotification(for : newEvent)
+            self?.events.sort { $0.date < $1.date }
+            StorageManager.shared.saveEvent(event: newEvent)
+            Notifications.shared.scheduleNotification(for: newEvent)
             self?.tableView.reloadData()
         }
         navigationController?.pushViewController(vc, animated: true)
@@ -53,7 +54,7 @@ class EventListController: UIViewController,  UITableViewDataSource, UITableView
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         events.count
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         let event = events[indexPath.row]
@@ -67,6 +68,6 @@ class EventListController: UIViewController,  UITableViewDataSource, UITableView
         formatter.timeStyle = .short
         return formatter.string(from: date)
     }
-
+    
     
 }
