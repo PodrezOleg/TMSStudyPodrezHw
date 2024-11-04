e499914a213f1c73b3943b8b9ccc5ae70bfb05f4
PodrezOleg <bolynita@icloud.com>
2024-11-04 19:27:24 +0300

    start

---
+++ PodrezHW34/PodrezHW34/Views/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PodrezHW34
+//
+//  Created by Oleg Podrez on 3.11.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
