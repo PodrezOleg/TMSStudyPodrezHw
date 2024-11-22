c2bbd6b333e0f6fc7937618150080385f7ce12f0
PodrezOleg <bolynita@icloud.com>
2024-11-22 22:21:01 +0300

    some checking - no result

--- PodrezHW46/PodrezHW46/View/SquareView.swift
+++ PodrezHW46/PodrezHW46/View/SquareView.swift
@@ -7,12 +7,63 @@
 
 import SwiftUI
 
-struct squareView: View {
+struct SquareView: View {
     var body: some View {
-        Text("Hello Square")
+        
+        ZStack{
+            Color.yellow
+                .ignoresSafeArea()
+            
+            Rectangle()
+                .fill(Color.white)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .padding(.top, 40)
+                .padding(.bottom, 40 )
+                .padding(.horizontal, 20)
+            
+            VStack {
+                
+                Spacer()
+                
+                Text("Wine Dishes")
+                    .font(.largeTitle)
+                    .bold()
+                    .padding()
+                Text("Balcan")
+                    .font(.footnote)
+                    .foregroundColor(.gray)
+                
+                Spacer()
+                
+                Image("wine")
+                    .resizable()
+                    .frame(minWidth: 200, minHeight:300)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .scaledToFit()
+                    .padding(.horizontal, 20)
+                
+                Text("$25")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(.black)
+                    .padding()
+                
+                Button(action: {
+                    print("buy wine")
+                }){
+                    Text("Buy")
+                        .font(.headline)
+                        .foregroundColor(.black)
+                        .frame(width: 100, height: 50)
+                        .background(Color.yellow)
+                        .cornerRadius(25)
+                }
+                Spacer()
+            }
+        }
     }
 }
 
     #Preview {
-        squareView()
+        SquareView()
     }
