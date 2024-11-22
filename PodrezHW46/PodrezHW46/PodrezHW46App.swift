fc7ea50bb2440931257eb1116e7736715e0160ae
PodrezOleg <bolynita@icloud.com>
2024-11-22 11:53:47 +0300

    arranged 3 views

---
+++ PodrezHW46/PodrezHW46/PodrezHW46App.swift
@@ -0,0 +1,17 @@
+//
+//  PodrezHW46App.swift
+//  PodrezHW46
+//
+//  Created by Oleg Podrez on 22.11.24.
+//
+
+import SwiftUI
+
+@main
+struct PodrezHW46App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PodrezHW46/PodrezHW46/View/SquareView.swift
@@ -0,0 +1,18 @@
+//
+//  SquareView.swift
+//  PodrezHW46
+//
+//  Created by Oleg Podrez on 22.11.24.
+//
+
+import SwiftUI
+
+struct squareView: View {
+    var body: some View {
+        Text("Hello Square")
+    }
+}
+
+    #Preview {
+        squareView()
+    }
