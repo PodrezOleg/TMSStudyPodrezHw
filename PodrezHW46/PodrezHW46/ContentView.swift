a65e9f2ea69baa391dba756b358dc0a5a82c0e5b
PodrezOleg <bolynita@icloud.com>
2024-11-23 13:33:11 +0300

    took a different path did not manage to resolve the problem with TabBar made some buttons. HW 46 Done

--- PodrezHW46/PodrezHW46/ContentView.swift
+++ PodrezHW46/PodrezHW46/ContentView.swift
@@ -7,37 +7,47 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    let tabs = ["Звезда", "Круг", "Квадрат"]
-    let icon = ["star.fill", "circle.fill", "square.fill"]
+struct TabElements {
+    let title: String
+    let icon: String
+}
 
+struct ContentView: View {
+    let tabs: [TabElements] = [
+
+        TabElements(title: "Круг", icon: "circle.fill"),
+        TabElements(title: "Квадрат", icon: "square.fill"),
+        TabElements(title: "Звезда", icon: "star.fill")
+    ]
+    
     init() {
         UITabBar.appearance().backgroundColor = UIColor.quaternarySystemFill
-       }
-    var body: some View {
-        
-        TabView {
-            StarView()
+    }
+    
+     var body: some View {
+         TabView {
+            ForEach(Array(tabs.enumerated()), id: \.offset) { index, tab in
+                getTabView(for: index)
                     .tabItem {
-                        Image(systemName: icon[0])
-                        Text(tabs[0])
+                        Image(systemName: tab.icon)
+                        Text(tab.title)
                     }
-            
-            CircleView()
-                .tabItem {
-                    Image(systemName: icon[1])
-                    Text(tabs[1])
-                }
-            
-            SquareView()
-                .tabItem {
-                    Image(systemName: icon[2])
-                    Text(tabs[2])
-                }
             }
         }
     }
-
-#Preview {
-    ContentView()
+    
+    @ViewBuilder private func getTabView(for index: Int) -> some View {
+        switch index {
+        
+        case 0: CircleView()
+        case 1: SquareView()
+        case 2: StarView()
+        default:
+            Text("Error")
+        }
+    }
+    
+    #Preview {
+        ContentView()
+    }
 }
