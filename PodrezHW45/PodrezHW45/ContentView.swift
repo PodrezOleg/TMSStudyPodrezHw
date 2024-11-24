d21d39f9f294a879b7920240f20d9fb08fdaf7d4
PodrezOleg <bolynita@icloud.com>
2024-11-25 00:10:19 +0300

    How it is done )

--- PodrezHW45/PodrezHW45/ContentView.swift
+++ PodrezHW45/PodrezHW45/ContentView.swift
@@ -11,13 +11,13 @@ import Charts
 struct ContentView: View {
     
     @State  var data: [ChartData] = [
-        ChartData(value: 5, label: "Mon"),
-        ChartData(value: 10, label: "Tue"),
-        ChartData(value: 95, label: "Wed"),
-        ChartData(value: 20, label: "Thu"),
-        ChartData(value: 79, label: "Fri"),
-        ChartData(value: 30, label: "Sat"),
-        ChartData(value: 35, label: "Sun")
+        ChartData(value: 5, label: "Monday"),
+        ChartData(value: 10, label: "Tuesday"),
+        ChartData(value: 95, label: "Wednesday"),
+        ChartData(value: 20, label: "Thursday"),
+        ChartData(value: 79, label: "Friday"),
+        ChartData(value: 30, label: "Saturday"),
+        ChartData(value: 35, label: "Sunday")
     ]
     var body: some View {
         VStack {
@@ -50,39 +50,48 @@ struct DataRowView: View {
     @Binding var data: ChartData
     
     var body: some View {
-        HStack (alignment: .center) {
-            
-            Button(action: {
-                data.value -= 1
-            }) {
-                Text("-")
-                    .frame(width: 10, height: 20)
-                    .font(.title)
-                    .padding()
-                    .foregroundColor(.white)
-                    .background(Color.red)
-                    .cornerRadius(10)
+       
+            HStack  {
+                Spacer()
+                Button(action: {
+                    data.value -= 1
+                }) {
+                    Text("-")
+                   
+                        .frame(width: 30, height: 30)
+                        .font(.title)
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.red)
+                        .cornerRadius(10)
+                }
+                
+                VStack {
                     
+                    Text("\(data.label): \(String(format: "%.0f", data.value))")
+                        .frame(maxWidth: 150)
+                        .font(.body)
+                        .lineLimit(1)
+                        .minimumScaleFactor(0.5)
+                        .padding(.vertical)
+                    
+                }
+                
+                Button(action: {
+                    data.value += 1
+                }) {
+                    Text("+")
+                        .frame(width: 30, height: 30)
+                        .font(.title)
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                }
+                Spacer()
             }
-            Text("\(data.label): \(String(format: "%.0f", data.value))")
-                .font(.body)
-                .padding(.horizontal)
-            
-            Button(action: {
-                data.value += 1
-            }) {
-                Text("+")
-                    .frame(width: 10, height: 20)
-                    .font(.title)
-                    .padding()
-                    .foregroundColor(.white)
-                    .background(Color.blue)
-                    .cornerRadius(10)
-            }
-        }
-    
     }
-    
+
 }
     
 struct ChartData: Identifiable {
