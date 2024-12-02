fed45c4b2869276627b74f9be67fb6f7b5809812
Oleg Podrez <bolynita@icloud.com>
2024-12-02 18:56:45 +0300

    HW 46 the tabView now it is working

--- PodrezHW46/PodrezHW46/ContentView.swift
+++ PodrezHW46/PodrezHW46/ContentView.swift
@@ -7,47 +7,28 @@
 
 import SwiftUI
 
-struct TabElements {
-    let title: String
-    let icon: String
-}
-
 struct ContentView: View {
-    let tabs: [TabElements] = [
 
-        TabElements(title: "Круг", icon: "circle.fill"),
-        TabElements(title: "Квадрат", icon: "square.fill"),
-        TabElements(title: "Звезда", icon: "star.fill")
-    ]
-    
     init() {
         UITabBar.appearance().backgroundColor = UIColor.quaternarySystemFill
     }
     
-     var body: some View {
-         TabView {
-            ForEach(Array(tabs.enumerated()), id: \.offset) { index, tab in
-                getTabView(for: index)
-                    .tabItem {
-                        Image(systemName: tab.icon)
-                        Text(tab.title)
-                    }
+    var body: some View {
+        TabView {
+            Tab("Circle", systemImage: "circle.fill") {
+                CircleView()
+            }
+            Tab("Sqyare", systemImage: "square.fill") {
+                SquareView()
+            }
+            Tab("Star", systemImage: "star.fill") {
+                StarView()
             }
         }
     }
-    
-    @ViewBuilder private func getTabView(for index: Int) -> some View {
-        switch index {
         
-        case 0: CircleView()
-        case 1: SquareView()
-        case 2: StarView()
-        default:
-            Text("Error")
+        #Preview {
+            ContentView()
         }
     }
-    
-    #Preview {
-        ContentView()
-    }
-}
+
--- PodrezHW46/PodrezHW46/PodrezHW46App.swift
+++ PodrezHW46/PodrezHW46/PodrezHW46App.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct PodrezHW46App: App {
     var body: some Scene {
         WindowGroup {
-            MainViewButtons()
+            ContentView()
         }
     }
 }
