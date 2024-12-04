a426e84e10f7f12cf12b9b770c5c93a82a4b425b
PodrezOleg <bolynita@icloud.com>
2024-12-04 13:40:38 +0300

    Update .gitignore to exclude Xcode metadata

--- PodrezHW46/PodrezHW46/ContentView.swift
+++ PodrezHW46/PodrezHW46/ContentView.swift
@@ -8,13 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+<<<<<<< Updated upstream
 
+=======
+    let tabs: [TabElements] = [
+        
+        TabElements(title: "Круг", icon: "circle.fill"),
+        TabElements(title: "Квадрат", icon: "square.fill"),
+        TabElements(title: "Звезда", icon: "star.fill")
+    ]
+    
+>>>>>>> Stashed changes
     init() {
         UITabBar.appearance().backgroundColor = UIColor.quaternarySystemFill
     }
     
     var body: some View {
         TabView {
+<<<<<<< Updated upstream
             Tab("Circle", systemImage: "circle.fill") {
                 CircleView()
             }
@@ -31,3 +42,32 @@ struct ContentView: View {
             ContentView()
         }
     }
+=======
+            ForEach(Array(tabs.enumerated()), id: \.element.icon) { index, tab in
+                getTabView(for: index)
+                    .tabItem {
+                        Label(tab.title, systemImage: tab.icon)
+                    }
+            }
+        }
+        
+    }
+        @ViewBuilder private func getTabView(for index: Int) -> some View {
+        
+            switch index {
+                
+            case 0: CircleView()
+            case 1: SquareView()
+            case 2: StarView()
+            default:
+                Text("Error")
+            }
+        }
+    }
+
+
+        #Preview {
+            ContentView()
+        }
+    
+>>>>>>> Stashed changes
